a1a6dce931b89ced0d7110bbc0239c50ff84b308
nasrul-hasan <nasrulhasan2010@gmail.com>
2025-05-31 20:14:01 +0600

    change the config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # config.py
 
-DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/task_db'
+#DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/task_db'
+import os
+
+DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/task_db")
 
 
